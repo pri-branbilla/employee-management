7831207472586ca4c2000bd1c73ace60b6eb315a
Priscilla Nascimento <priscillans2712@gmail.com>
2019-07-06 03:13:56 -0300

    setup firebase auth

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import reducers from './reducers'
 
 
 const App = () => (
-  <Provider store={createStore()}>
+  <Provider store={createStore(reducers)}>
     <View>
       <Text>Hello!</Text>
     </View>
