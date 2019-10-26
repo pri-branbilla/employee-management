4045b9da8e5fa67f62fbd556239d255ac7ad0b5d
Priscilla Nascimento <priscillans2712@gmail.com>
2019-10-26 18:27:23 -0300

    update podfile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import * as firebase from 'firebase'
 import ReduxThunk from 'redux-thunk'
 import { Provider } from 'react-redux'
@@ -6,8 +6,8 @@ import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers'
 import Router from './Router'
 
-class App extends Component {
-  componentDidMount = () => {
+const App = () => {
+  useEffect(() => {
     firebase.initializeApp({
       apiKey: 'AIzaSyAc_hGLjVYBLNS1h4pNiMRSI72A2pOv6Mw',
       authDomain: 'rnauth-d23ab.firebaseapp.com',
@@ -17,9 +17,9 @@ class App extends Component {
       messagingSenderId: '873867629031',
       appId: '1:873867629031:web:dd72bf2b72045b67',
     })
-  }
+  }, [])
 
-  render = () => (
+  return (
     <Provider store={
       createStore(reducers, {}, applyMiddleware(ReduxThunk))
     }
